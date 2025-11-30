dbdc4638a0a93ea1e3639a76f357916b7042a7e2
Iván Villanueva Sabalete <villanueva_i1@hotmail.com>
2025-11-30 20:36:02 +0100

    Update social media icons in helper.R

--- R/helper.R
+++ R/helper.R
@@ -19,7 +19,7 @@ if (!dir.exists("png")) dir.create("png")
 
 
 # Información del autor para el pie de gráfico
-twitter <- "<i class="fa-brands fa-square-x-twitter"></i>"
+twitter <- '<span class="fa-brands fa-twitter" style="color:#000000;" aria-hidden="true"></span>'
 tweetelcheff <- "<span>*@elcheff*</span>"
 insta <- "<span style='color:#E1306C;font-family: \"Font Awesome 6 Brands\"'>&#xE055;</span>"
 instaelcheff <- "<span>*@sport_iv0*</span>"
